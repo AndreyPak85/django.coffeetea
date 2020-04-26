b61ba78a7476f138ddef86817807e67458ac1de3
Andrey Pak <andrey.pak85@gmail.com>
2020-04-26 20:07:16 +0600

    add model Coffee

--- start/models.py
+++ start/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Coffee(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
